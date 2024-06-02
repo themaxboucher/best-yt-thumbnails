455ce881ebcf3307a380711068e7911b3e0c3818
Maxime Boucher <maxime@maximeboucher.com>
2024-06-02 11:15:12 +0200

    style: fix header grid background

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -10,8 +10,8 @@ export default function MainLayout({ children }) {
       <ModalsContextProvider>
         <Navbar />
         <main className="relative">
-          <div className="absolute inset-0 -z-30 h-2/5 w-full bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-[size:40px_40px]"></div>
-          <div className="absolute inset-0 -z-20 h-2/5 w-full bg-gradient-to-t from-white to-transparent to-50%"></div>
+          <div className="absolute inset-0 -z-30 h-56 w-full bg-[linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)] bg-[size:40px_40px]"></div>
+          <div className="absolute inset-0 -z-20 h-56 w-full bg-gradient-to-t from-white to-transparent to-50%"></div>
           {children}
         </main>
         <Footer />
