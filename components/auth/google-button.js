4de505b8aa34768942c9bc08f0cf46c10f17ff05
Maxime Boucher <maxime@maximeboucher.com>
2024-06-08 19:55:03 +0200

    fix: close auth modal once user is authenticated

--- components/auth/google-button.js
+++ components/auth/google-button.js
@@ -4,13 +4,18 @@ import Button from "../ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { auth } from "@/data/firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useContext } from "react";
+import { ModalsContext } from "@/store/modals-context";
 
 export default function GoogleButton() {
+  const useModalsContext = useContext(ModalsContext);
+
   // Sign in with google
   const googleProvider = new GoogleAuthProvider();
   const googleLoginHandler = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
+      useModalsContext.closeAuthModal();
       console.log(result.user);
     } catch (error) {
       console.log(error);
